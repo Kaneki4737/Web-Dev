c6a1e4221612bbce82be3ef73b9a464d0f907ffd
ken <dabhane2006@gmail.com>
2025-06-23 13:26:18 +0530

    Division, square, and square root

--- calc.py
+++ calc.py
@@ -7,3 +7,13 @@ class calc:
 
     def mult(a,b):
         return a*b
+    
+    def dev(a,b):
+        return a/b
+    
+    def square(a):
+        return a**2
+    
+    def sqrt(a):
+        return a**0.5
+    
