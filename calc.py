9418f3c66f26cabb784dc88a89d5949e09939170
ken <dabhane2006@gmail.com>
2025-06-23 13:24:02 +0530

    Class feature

--- calc.py
+++ calc.py
@@ -1,14 +1,9 @@
-def add(a,b):
-    return a+b
+class calc:
+    def add(a,b):
+        return a+b
 
-print(add(4,5))
+    def sub(a,b):
+        return a-b
 
-def sub(a,b):
-    return a-b
-
-print(sub(5,4))
-
-def mult(a,b):
-    return a*b
-
-print(mult(5,5))
+    def mult(a,b):
+        return a*b
